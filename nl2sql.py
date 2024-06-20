1906ef9e5497352d1d9a34bd9fbfdd93e159ed61
Kerwin <141232455+milkfish033@users.noreply.github.com>
2024-06-20 10:59:53 +0800

    Create nl2sql.py

---
+++ nl2sql.py
@@ -0,0 +1,222 @@
+
+import llama_index.core
+from llama_index.core.query_pipeline import  QueryPipeline as QP
+from llama_index.core.agent import QueryPipelineAgentWorker
+from llama_index.core.settings import Settings
+from llama_index.core.callbacks import CallbackManager
+from llama_index.core import SQLDatabase
+from sqlalchemy import (create_engine,)
+from llama_index.core.query_engine import NLSQLTableQueryEngine
+from llama_index.core import SQLDatabase
+from llama_index.core.tools import QueryEngineTool
+import phoenix as px
+from llama_index.core.agent.react.types import (
+    ActionReasoningStep,
+    ObservationReasoningStep,
+    ResponseReasoningStep,
+)
+from llama_index.core.agent import Task, AgentChatResponse
+from llama_index.core.query_pipeline import (
+    AgentInputComponent,
+    AgentFnComponent,
+    ToolRunnerComponent,
+)
+from llama_index.core.llms import MessageRole
+from typing import Dict, Any, List, cast
+from llama_index.core.agent import ReActChatFormatter
+from llama_index.core.llms import ChatMessage
+from llama_index.core.tools import BaseTool
+
+from typing import Set, Optional
+from llama_index.core.agent.react.output_parser import ReActOutputParser
+from llama_index.core.llms import ChatResponse
+from llama_index.core.agent.types import Task
+from llama_index.core.query_pipeline import QueryPipeline as QP
+
+
+# define global callback setting
+callback_manager = CallbackManager()
+Settings.callback_manager = callback_manager
+
+# setup Arize Phoenix for logging/observability
+px.launch_app()
+llama_index.core.set_global_handler("arize_phoenix")
+
+
+#Specify your own database URL here 
+engine = create_engine("*******")
+sql_database = SQLDatabase(engine)
+
+sql_query_engine = NLSQLTableQueryEngine(
+    sql_database=sql_database,
+    tables=["albums", "tracks", "artists"],
+    verbose=True,
+)
+
+sql_tool = QueryEngineTool.from_defaults(
+    query_engine=sql_query_engine,
+    name="sql_tool",
+    description=(
+        "Useful for translating a natural language query into a SQL query"
+    ),
+)
+
+qp = QP(verbose=True)
+
+#Specify your own AI Ark here
+from volcenginesdkarkruntime import Ark
+client = Ark(ak="*****", sk="****")
+
+## Agent Input Component
+## This is the component that produces agent inputs to the rest of the components
+## Can also put initialization logic here.
+def agent_input_fn(task: Task, state: Dict[str, Any]) -> Dict[str, Any]:
+    """Agent input function.
+
+    Returns:
+        A Dictionary of output keys and values. If you are specifying
+        src_key when defining links between this component and other
+        components, make sure the src_key matches the specified output_key.
+
+    """
+    # initialize current_reasoning
+    if "current_reasoning" not in state:
+        state["current_reasoning"] = []
+    reasoning_step = ObservationReasoningStep(observation=task.input)
+    state["current_reasoning"].append(reasoning_step)
+    return {"input": task.input}
+
+
+agent_input_component = AgentInputComponent(fn=agent_input_fn)
+
+## define prompt function
+def react_prompt_fn(
+    task: Task, state: Dict[str, Any], input: str, tools: List[BaseTool]
+) -> List[ChatMessage]:
+    # Add input to reasoning
+    chat_formatter = ReActChatFormatter()
+    return chat_formatter.format(
+        tools,
+        chat_history=task.memory.get() + state["memory"].get_all(),
+        current_reasoning=state["current_reasoning"],
+    )
+
+
+react_prompt_component = AgentFnComponent(
+    fn=react_prompt_fn, partial_dict={"tools": [sql_tool]}
+)
+
+def parse_react_output_fn(
+    task: Task, state: Dict[str, Any], chat_response: ChatResponse
+):
+    """Parse ReAct output into a reasoning step."""
+    output_parser = ReActOutputParser()
+    reasoning_step = output_parser.parse(chat_response.message.content)
+    return {"done": reasoning_step.is_done, "reasoning_step": reasoning_step}
+
+
+parse_react_output = AgentFnComponent(fn=parse_react_output_fn)
+
+
+def run_tool_fn(
+    task: Task, state: Dict[str, Any], reasoning_step: ActionReasoningStep
+):
+    """Run tool and process tool output."""
+    tool_runner_component = ToolRunnerComponent(
+        [sql_tool], callback_manager=task.callback_manager
+    )
+    tool_output = tool_runner_component.run_component(
+        tool_name=reasoning_step.action,
+        tool_input=reasoning_step.action_input,
+    )
+    observation_step = ObservationReasoningStep(observation=str(tool_output))
+    state["current_reasoning"].append(observation_step)
+    # TODO: get output
+
+    return {"response_str": observation_step.get_content(), "is_done": False}
+
+
+run_tool = AgentFnComponent(fn=run_tool_fn)
+
+
+def process_response_fn(
+    task: Task, state: Dict[str, Any], response_step: ResponseReasoningStep
+):
+    """Process response."""
+    state["current_reasoning"].append(response_step)
+    response_str = response_step.response
+    # Now that we're done with this step, put into memory
+    state["memory"].put(ChatMessage(content=task.input, role=MessageRole.USER))
+    state["memory"].put(
+        ChatMessage(content=response_str, role=MessageRole.ASSISTANT)
+    )
+
+    return {"response_str": response_str, "is_done": True}
+
+
+process_response = AgentFnComponent(fn=process_response_fn)
+
+
+def process_agent_response_fn(
+    task: Task, state: Dict[str, Any], response_dict: dict
+):
+    """Process agent response."""
+    return (
+        AgentChatResponse(response_dict["response_str"]),
+        response_dict["is_done"],
+    )
+
+
+process_agent_response = AgentFnComponent(fn=process_agent_response_fn)
+
+qp.add_modules(
+    {
+        "agent_input": agent_input_component,
+        "react_prompt": react_prompt_component,
+        "llm": 'ep-20240523031819-glvjx',
+        "react_output_parser": parse_react_output,
+        "run_tool": run_tool,
+        "process_response": process_response,
+        "process_agent_response": process_agent_response,
+    }
+)
+
+# link input to react prompt to parsed out response (either tool action/input or observation)
+qp.add_chain(["agent_input", "react_prompt", "llm", "react_output_parser"])
+
+# add conditional link from react output to tool call (if not done)
+qp.add_link(
+    "react_output_parser",
+    "run_tool",
+    condition_fn=lambda x: not x["done"],
+    input_fn=lambda x: x["reasoning_step"],
+)
+# add conditional link from react output to final response processing (if done)
+qp.add_link(
+    "react_output_parser",
+    "process_response",
+    condition_fn=lambda x: x["done"],
+    input_fn=lambda x: x["reasoning_step"],
+)
+
+# whether response processing or tool output processing, add link to final agent response
+qp.add_link("process_response", "process_agent_response")
+qp.add_link("run_tool", "process_agent_response")
+
+from pyvis.network import Network
+
+net = Network(notebook=True, cdn_resources="in_line", directed=True)
+net.from_nx(qp.clean_dag)
+net.show("agent_dag.html")
+
+agent_worker = QueryPipelineAgentWorker(qp)
+agent = agent_worker.as_agent(
+    callback_manager=CallbackManager([]), verbose=True
+)
+
+task = agent.create_task(
+    "how many people were alive yesterday"
+)
+
+
+step_output = agent.run_step(task.task_id)
